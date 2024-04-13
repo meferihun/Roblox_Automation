0ea269e5d308923178620f1229acf1a7280623eb
meferihun <meferike@gmail.com>
2024-04-13 09:17:32 +0200

    Made fishing faster

--- Dungeon Quest/leeching.py
+++ Dungeon Quest/leeching.py
@@ -5,7 +5,7 @@ from PIL import ImageGrab
 def recognizeRetryColor(desiredColor, width: int, height: int):
     px = ImageGrab.grab().load()
 
-    for y in range(int(height * (1 / 3)), int(height * (3 / 4))):
+    for y in range(int(height * (1 / 3)), height):
         for x in range(0, width):
             if px[x, y] == desiredColor:
                 if len(retry_pos) == 0:
@@ -16,12 +16,12 @@ def recognizeRetryColor(desiredColor, width: int, height: int):
 def recognizeBossColor(desiredColor, width: int, height: int):
     px = ImageGrab.grab().load()
 
-    for y in range(int(height * (1 / 3)), int(height * (3 / 4))):
+    for y in range(int(height * (1 / 3)), height):
         for x in range(0, width):
             if px[x, y] == desiredColor:
                 if len(boss_pos) == 0:
                     boss_pos.append([x, y])
-            return True
+                return True
 
 
 width = pyautogui.size()[0]
@@ -34,9 +34,11 @@ boss_pos = []
 while True:
 
     if recognizeRetryColor(retry, width, height) and recognizeBossColor(boss, width, height):
-        pyautogui.moveTo(boss_pos[0][0], boss_pos[0][1], 1)
-        pyautogui.click(clicks=2, interval=0.5)
+        pyautogui.moveTo(boss_pos[0][0]+5, boss_pos[0][1]+5, 1)
+        pyautogui.sleep(1)
+        pyautogui.click(clicks=4, interval=0.5)
 
     elif recognizeRetryColor(retry, width, height) and not recognizeBossColor(boss, width, height):
-        pyautogui.moveTo(retry_pos[0][0], retry_pos[0][1], 1)
-        pyautogui.click(clicks=2, interval=0.5)
+        pyautogui.moveTo(retry_pos[0][0]+5, retry_pos[0][1]+5, 1)
+        pyautogui.sleep(1)
+        pyautogui.click(clicks=4, interval=1)
